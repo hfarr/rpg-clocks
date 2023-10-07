bf5e2a13d11eb2e325abe39e3147f8b8b5cee4a7
Henry Farr <hfarr@hey.com>
2023-10-07 13:17:31 -0700

    Add interactivity to the clock

--- src/clock/clock.tsx
+++ src/clock/clock.tsx
@@ -3,6 +3,8 @@ import * as _ from "lodash"
 
 type SegmentProps = {
   active: boolean
+  // segmentNumber: number
+  onClick: () => void
 
   // temp
   sideLength?: string 
@@ -10,10 +12,13 @@ type SegmentProps = {
 const Segment = (props: SegmentProps) => {
   const {
     active,
-    sideLength = "50px"
+    sideLength = "50px",
+    onClick,
   } = props
   const backgroundColor = active ? "#00aa55" : "#333"
-  return <div data-cl={active} style={{ width: sideLength, height: sideLength, backgroundColor }}>
+  return <div data-cl={active} 
+    style={{ width: sideLength, height: sideLength, backgroundColor }}
+    onClick={onClick}>
   </div>
 }
 
@@ -35,8 +40,35 @@ const Clock = (props: ClockProps) => {
     color = "#"
   } = props
 
-  return <div style={{ display: 'flex' }}>
-    {_.range(segments).map( segmentNumber => <Segment key={segmentNumber} active={progress > segmentNumber} /> )}
+  const [ currentProgress, setCurrentProgress ] = React.useState(progress)
+
+  const updateProgress = (segmentNumber: number) => {
+
+    // progress = 2
+    // x  x
+    // 0, 1, 2, 3, 4, 5
+    //
+    // click 2 -> progress = 3
+    // x  x  x
+    // 0, 1, 2, 3, 4, 5
+    //
+    // click 2 -> progress = 2
+    // x  x
+    // 0, 1, 2, 3, 4, 5
+    const isActiveSegment = segmentNumber < currentProgress;
+
+    const newProgress = isActiveSegment ? segmentNumber : segmentNumber + 1
+    console.log(`Current progress: ${currentProgress}. Click segment ${segmentNumber}. New progress: ${newProgress}`)
+
+    setCurrentProgress(newProgress)
+  }
+
+  return <div style={{ display: 'flex' }} >
+    {_.range(segments).map( segmentNumber => 
+      <Segment key={segmentNumber} 
+        active={currentProgress > segmentNumber} 
+        onClick={ () => updateProgress(segmentNumber) } /> 
+    )}
   </div>
 }
 
